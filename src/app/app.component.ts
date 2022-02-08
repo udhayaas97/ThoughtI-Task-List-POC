8a13b12f37b0d2723c3ddff37aedebcd219372cf
udhayaas97@gmail.com <udhayaas97@gmail.com>
2022-02-08 13:24:50 +0530

    (feat) : UI for task from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faFlag as farFlag } from '@fortawesome/free-regular-svg-icons';
+import { faFlag as fasFlag } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
+import { TaskService } from './task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  farFlag = farFlag;
+  fasFlag = fasFlag;
+  isImportant = true;
+  users: string[] = [];
+  private subscriptions = new Subscription();
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    const observer = this.taskService.UserList.subscribe((users) => {
+      this.users = users;
+    });
+    this.subscriptions.add(observer);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  onImportantSelect(): void {
+    this.isImportant = !this.isImportant;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
